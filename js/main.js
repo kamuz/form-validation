0362613e99ae335970b2a517ed6c3abb4d1baf70
Vladimir Kamuz <v.kamuz@gmail.com>
2022-04-03 23:42:58 +0300

    Validate Zip Code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
 
 function validateName(){
 	const name = document.getElementById('name');
@@ -8,5 +9,16 @@ function validateName(){
 		name.classList.add('is-invalid');
 	} else {
 		name.classList.remove('is-invalid');
+	}
+}
+
+function validateZip(){
+	const zip = document.getElementById('zip');
+	const re = /^[0-9]{5}(-[0-9]{4})?$/;
+
+	if(!re.test(zip.value)){
+		zip.classList.add('is-invalid');
+	} else {
+		zip.classList.remove('is-invalid');
 	}
 }
